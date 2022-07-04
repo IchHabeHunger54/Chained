5cab98443b1c180e5ecc6197d531b5b8e051dae8
IchHabeHunger54 <kingdavidaut@gmail.com>
2022-07-04 18:11:00 +0200

    add netherite horse armor

--- src/main/java/ihh/chained/Chained.java
+++ src/main/java/ihh/chained/Chained.java
@@ -8,12 +8,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod("chained")
+@Mod(Chained.MOD_ID)
 public class Chained {
+    public static final String MOD_ID = "chained";
+
     public Chained() {
-        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "chained");
+        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Chained.MOD_ID);
         ITEMS.register("chainmail", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("chainmail_horse_armor", () -> new HorseArmorItem(4, "chainmail", new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+        ITEMS.register("netherite_horse_armor", () -> new HorseArmorItem(12, "netherite", new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1).fireResistant()));
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 }
