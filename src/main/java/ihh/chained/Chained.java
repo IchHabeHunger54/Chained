1ad191825b11c8723946fcddf72c7d89d2d42046
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-05-28 00:14:28 +0200

    1.19.4 update

--- src/main/java/ihh/chained/Chained.java
+++ src/main/java/ihh/chained/Chained.java
@@ -1,22 +1,35 @@
 package ihh.chained;
 
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.HorseArmorItem;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(Chained.MOD_ID)
 public class Chained {
     public static final String MOD_ID = "chained";
 
     public Chained() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Chained.MOD_ID);
-        ITEMS.register("chainmail", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
-        ITEMS.register("chainmail_horse_armor", () -> new HorseArmorItem(4, "chainmail", new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
-        ITEMS.register("netherite_horse_armor", () -> new HorseArmorItem(12, "netherite", new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1).fireResistant()));
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        RegistryObject<Item> chainmail = ITEMS.register("chainmail", () -> new Item(new Item.Properties()));
+        RegistryObject<HorseArmorItem> chainmailHorseArmor = ITEMS.register("chainmail_horse_armor", () -> new HorseArmorItem(4, "chainmail", new Item.Properties().stacksTo(1)));
+        RegistryObject<HorseArmorItem> netheriteHorseArmor = ITEMS.register("netherite_horse_armor", () -> new HorseArmorItem(12, "netherite", new Item.Properties().stacksTo(1).fireResistant()));
+        ITEMS.register(bus);
+        bus.<CreativeModeTabEvent.BuildContents>addListener(e -> {
+            if (e.getTab() == CreativeModeTabs.INGREDIENTS) {
+                e.accept(chainmail);
+            }
+            if (e.getTab() == CreativeModeTabs.COMBAT) {
+                e.accept(chainmailHorseArmor);
+                e.accept(netheriteHorseArmor);
+            }
+        });
     }
 }
