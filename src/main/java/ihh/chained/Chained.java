65a35e316dde7068e2264f4e9f07a632732697c5
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-09-28 18:06:43 +0200

    update to 1.21.1

--- src/main/java/ihh/chained/Chained.java
+++ src/main/java/ihh/chained/Chained.java
@@ -1,34 +1,24 @@
 package ihh.chained;
 
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.HorseArmorItem;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(Chained.MOD_ID)
 public class Chained {
     public static final String MOD_ID = "chained";
 
-    public Chained() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Chained.MOD_ID);
-        RegistryObject<Item> chainmail = ITEMS.register("chainmail", () -> new Item(new Item.Properties()));
-        RegistryObject<HorseArmorItem> chainmailHorseArmor = ITEMS.register("chainmail_horse_armor", () -> new HorseArmorItem(4, "chainmail", new Item.Properties().stacksTo(1)));
-        RegistryObject<HorseArmorItem> netheriteHorseArmor = ITEMS.register("netherite_horse_armor", () -> new HorseArmorItem(12, "netherite", new Item.Properties().stacksTo(1).fireResistant()));
+    public Chained(IEventBus bus) {
+        DeferredRegister.Items ITEMS = DeferredRegister.createItems(Chained.MOD_ID);
+        DeferredItem<Item> chainmail = ITEMS.registerSimpleItem("chainmail");
         ITEMS.register(bus);
         bus.<BuildCreativeModeTabContentsEvent>addListener(e -> {
             if (e.getTabKey() == CreativeModeTabs.INGREDIENTS) {
                 e.accept(chainmail);
-            }
-            if (e.getTabKey() == CreativeModeTabs.COMBAT) {
-                e.accept(chainmailHorseArmor);
-                e.accept(netheriteHorseArmor);
             }
         });
     }
